47ce7e514f90e4e2d4f74a5bed23ec4780ef2bba
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-11-13 11:28:20 +0100

    Update queue driver default value

--- config/queue.php
+++ config/queue.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default' => env('QUEUE_DRIVER', env('QUEUE_DRIVER')),
+    'default' => env('QUEUE_DRIVER', 'sync'),
     'connections' => [
         'database' => [
             'driver' => 'database',
