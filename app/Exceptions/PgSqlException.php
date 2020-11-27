f6fae99003ee6c4f28db0402eed5f6ec8db8f360
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-11-27 10:53:11 +0100

    Add PGSQL Exception

---
+++ app/Exceptions/PgSqlException.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Exceptions;
+
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use PDOException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
+
+class PgSqlException extends PDOException
+{
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @return void
+     *
+     */
+    public function report()
+    {
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @return Response|JsonResponse
+     *
+     */
+    public function render()
+    {
+        if(strstr($this->getMessage(), 'PDOException: SQLSTATE[')) {
+            if(preg_match('/SQLSTATE\[(\w+)\]:(.*):(\s+\d)(.*):(.*)/', $this->getMessage(), $matches) === false) {
+                $this->message = 'Generic SQL exception unhandled';
+            }
+            else {
+                $this->message = empty($matches[5]) ? 'Generic SQL exception unhandled' : trim($matches[5]);
+            }
+        }
+
+        return response()->json(['message' => $this->getMessage()], 409);
+    }
+}
