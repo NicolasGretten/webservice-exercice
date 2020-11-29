4eea94b13cfd297761d4eb10125759389a822ddd
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-11-29 13:03:34 +0100

    Add JwtTrait.php

---
+++ app/Traits/JwtTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Crypt;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+trait JwtTrait
+{
+    /*
+     * Retrieve JWT Profile
+     */
+    public function getJwtProfile() {
+        $payload = JWTAuth::parseToken()->getPayload();
+
+        if(empty($payload->get('profiles'))) {
+            throw new JWTException('Profile payload not found in JWT token.', 409);
+        }
+
+        return (object) Crypt::decrypt($payload->get('profile'));
+    }
+}
