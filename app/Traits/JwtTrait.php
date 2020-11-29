56239cb0d1db9177ce510646c5ed4b88fa3da855
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-11-29 13:31:27 +0100

    Update exceptions in JwtTrait.php

--- app/Traits/JwtTrait.php
+++ app/Traits/JwtTrait.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use Exception;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Support\Facades\Crypt;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -12,12 +14,17 @@ trait JwtTrait
      * Retrieve JWT Profile
      */
     public function getJwtProfile() {
-        $payload = JWTAuth::parseToken()->getPayload();
+        try {
+            $payload = JWTAuth::parseToken()->getPayload();
 
-        if(empty($payload->get('profile'))) {
-            throw new JWTException('Profile payload not found in JWT token.', 409);
+            if(empty($payload->get('profiles'))) {
+                throw new JWTException('Profile payload not found in JWT token.', 409);
+            }
+
+            return (object) Crypt::decrypt($payload->get('profile'));
         }
-
-        return (object) Crypt::decrypt($payload->get('profile'));
+        catch(JWTException | DecryptException | Exception $e) {
+            throw $e;
+        }
     }
 }
