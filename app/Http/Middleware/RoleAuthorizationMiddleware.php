f6f399c55a04589391c2f2b7eb4a767c29abe70e
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-12-14 17:06:13 +0100

    Add external login from JWT

--- app/Http/Middleware/RoleAuthorizationMiddleware.php
+++ app/Http/Middleware/RoleAuthorizationMiddleware.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Http\Middleware;
 
+use App\Traits\JwtTrait;
 use Closure;
 
+use Illuminate\Support\Facades\Crypt;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
@@ -10,23 +12,35 @@ use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class RoleAuthorizationMiddleware
 {
+    use JwtTrait;
+
     public function handle($request, Closure $next, ...$roles)
     {
         try {
             $token = JWTAuth::parseToken();
             $user = $token->authenticate();
         }
-        catch (TokenExpiredException $e) {
+        catch(TokenExpiredException $e) {
             return $this->unauthorized();
         }
-        catch (TokenInvalidException $e) {
+        catch(TokenInvalidException $e) {
             return $this->unauthorized();
         }
-        catch (JWTException $e) {
+        catch(JWTException $e) {
             return $this->unauthorized();
         }
 
+        /*
+         * User logged from local database
+         */
         if ($user && sizeof(array_intersect(explode(',', $user->role), $roles))) {
+            return $next($request);
+        }
+
+        /*
+         * User logged from another API
+         */
+        if(sizeof(array_intersect(explode(',', $this->jwt('profile')->get('role')), $roles))) {
             return $next($request);
         }
 
