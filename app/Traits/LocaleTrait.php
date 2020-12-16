57f2722880155029871f53c1801a49990ce71b02
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-12-16 14:26:00 +0100

    Add locale trait

---
+++ app/Traits/LocaleTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Exception;
+
+trait LocaleTrait
+{
+    use JwtTrait;
+
+    public function setLocale()
+    {
+        if (!empty($this->jwt('profile')->get('locale'))) {
+            $locale = $this->jwt('profile')->get('locale');
+        } elseif (!empty(app('request')->headers->get('locale'))) {
+            $locale = app('request')->headers->get('locale');
+        } else {
+            $locale = env('LOCALES_ALLOWED');
+        }
+
+        self::checkRequestedLocale($locale);
+
+        app()->setLocale($locale);
+    }
+
+    public function checkRequestedLocale($locale)
+    {
+        $localesAllowed = explode(',', env('LOCALES_ALLOWED'));
+
+        if (!in_array($locale, $localesAllowed)) {
+            throw new Exception('Locale not allowed.');
+        }
+    }
+}
