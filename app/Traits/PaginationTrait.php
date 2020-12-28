1ae08b7b857d4955bdd3b6dd4740f1d2edd89332
alexandrelemale <alexandre.lemale@goworkandco.com>
2020-12-28 16:38:05 +0100

    Misc update

--- app/Traits/PaginationTrait.php
+++ app/Traits/PaginationTrait.php
@@ -20,6 +20,7 @@ trait PaginationTrait
         if(!empty($limit) and (int) $limit > 0) {
             $pagination = $builder->paginate($limit);
 
+            $pagination->setPath($this->makePath());
             $this->pagination = json_encode([
                 'current_page'  => (int) $pagination->currentPage(),
                 'last_page'     => (int) $pagination->lastPage(),
@@ -31,5 +32,24 @@ trait PaginationTrait
         }
 
         return $this;
+    }
+
+    private function makePath(): string
+    {
+        $parameters = [];
+        $explodedQueryString = explode('&', Request()->getQueryString());
+        foreach ($explodedQueryString as $string) {
+            $values = explode('=', $string);
+            $key = $values[0];
+            $val = $values[1];
+
+            if($key == '_url' or $key == 'page') {
+                continue;
+            }
+
+            $parameters[$key] = $val;
+        }
+
+        return Request()->input('_url') . '?' . http_build_query($parameters);
     }
 }
