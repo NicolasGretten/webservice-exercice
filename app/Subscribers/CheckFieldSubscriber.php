a0ef677005d3e0a10ac37121c26ef37981985824
alexandrelemale <alexandre.lemale@goworkandco.com>
2021-01-12 11:10:58 +0100

    Update PUBSUB service

--- app/Subscribers/CheckFieldSubscriber.php
+++ app/Subscribers/CheckFieldSubscriber.php
@@ -54,8 +54,8 @@ class CheckFieldSubscriber
                             ]
                         ]));
                     break;
-                */
                 }
+            */
             }
         }
         catch(JsonEncodingException | Exception $e) {
