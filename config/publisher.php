44963ae1f1a1ab9d1ec9d2f0ca090a38a8fad869
alexandrelemale <alexandre.lemale@goworkandco.com>
2021-01-18 13:34:55 +0100

    adding PUB-SUB default publishers

--- config/publisher.php
+++ config/publisher.php
@@ -6,5 +6,8 @@ return [
         'secret' => env('PUBLISHER_SNS_SECRET'),
         'region' => env('PUBLISHER_SNS_REGION'),
     ],
-    'events' => []
+    'events' => [
+        'confirm_field'                                 => env('PUBLISHER_SNS_PREFIX'). ':confirm_field',
+        'check_field'                                   => env('PUBLISHER_SNS_PREFIX'). ':check_field'
+    ]
 ];
